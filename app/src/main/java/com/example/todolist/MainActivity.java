fd4000ef5f61fac1a0051a9829603b20cc644c60
nishant <email.nishant.singh@gmail.com>
2020-02-26 18:34:33 +0530

    --added a textView HomeScreen

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textViewHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewHome=findViewById(R.id.textViewHomeScreen);
     }
 }
