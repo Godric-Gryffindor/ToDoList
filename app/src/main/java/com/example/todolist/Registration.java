4ff242c9b58307daee6cf9b25d571764fe67e7e9
nishant <email.nishant.singh@gmail.com>
2020-02-28 10:23:07 +0530

    Added Firebase to the application --next commit will include the coding for the firebase authentication using email and password

--- app/src/main/java/com/example/todolist/Registration.java
+++ app/src/main/java/com/example/todolist/Registration.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Registration extends AppCompatActivity {
     TextView textViewSignIn;
@@ -37,11 +39,30 @@ public class Registration extends AppCompatActivity {
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                registration();
+              /*
                 Intent intent= new Intent(Registration.this,HomeScreen.class);
                 startActivity(intent);
                 finish();
+              */
             }
         });
 
+    }
+    private Boolean registration(){
+        String email,pass;
+        email=editTextEmail.getText().toString().trim();
+        pass=editTextPass.getText().toString().trim();
+        Log.d("Email", email);
+        Log.d("Password", pass);
+        if (email.isEmpty()){
+            Toast.makeText(this, "Enter your email to proceed ", Toast.LENGTH_SHORT).show();
+        }else if(pass.isEmpty()){
+            Toast.makeText(this, "Enter your password to proceed ", Toast.LENGTH_SHORT).show();
+        }else if (pass.length()<6){
+            Toast.makeText(this, "Password length must be more than 5 characters ", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
